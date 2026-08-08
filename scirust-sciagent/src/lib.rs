ae6f3825c58ee93a2cd2cba9bbc5665ba4153b94
ZEKRITI Tarek <194770978+CHECKUPAUTO@users.noreply.github.com>
2026-08-08 22:58:24 +0200

    feat(sciagent): export heap elastic BPE kernel

--- scirust-sciagent/src/lib.rs
+++ scirust-sciagent/src/lib.rs
@@ -11,6 +11,7 @@ pub mod corpus_paths;
 pub mod cuda_model;
 pub mod elastic_calibration;
 pub mod elastic_engine;
+pub mod elastic_heap;
 pub mod elastic_indexed;
 pub mod elastic_tiny;
 pub mod elastic_tokenizer;
@@ -38,6 +39,7 @@ pub use elastic_calibration::{
     CalibrationError, CalibrationMeasurement, CalibrationReport, CalibrationWinner,
 };
 pub use elastic_engine::{ElasticBpeEngine, ElasticEncoding};
+pub use elastic_heap::HeapBpe;
 pub use elastic_indexed::IndexedBpe;
 pub use elastic_tiny::{TINY_SCAN_CAPACITY, TinyScanBpe};
 pub use elastic_tokenizer::{
