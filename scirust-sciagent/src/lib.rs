a888aadab82dd2feeda8736f534964a0a8685503
MEMOPERF <contact@checkupauto.fr>
2026-08-08 23:04:45 +0200

    feat(sciagent): export persistent elastic profiles

--- scirust-sciagent/src/lib.rs
+++ scirust-sciagent/src/lib.rs
@@ -15,6 +15,7 @@ pub mod elastic_engine;
 pub mod elastic_heap;
 pub mod elastic_indexed;
 pub mod elastic_profile_fit;
+pub mod elastic_profile_store;
 pub mod elastic_tiny;
 pub mod elastic_tokenizer;
 pub mod flash_attention;
@@ -47,6 +48,10 @@ pub use elastic_engine::{ElasticBpeEngine, ElasticEncoding};
 pub use elastic_heap::HeapBpe;
 pub use elastic_indexed::IndexedBpe;
 pub use elastic_profile_fit::{ElasticProfileFitter, ProfileFitError};
+pub use elastic_profile_store::{
+    CANONICAL_BPE_SEMANTICS_V1, ELASTIC_PROFILE_SCHEMA_V1, ElasticHardwareIdentity,
+    ProfileStoreError, StoredElasticProfile, ordered_merges_fingerprint,
+};
 pub use elastic_tiny::{TINY_SCAN_CAPACITY, TinyScanBpe};
 pub use elastic_tokenizer::{
     BpeKernel, CanonicalBpeOracle, DuplicateMergeRule, ElasticProfile, ElasticThresholds,
